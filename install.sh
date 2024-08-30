495a94a0030fda26837aeec3212ce6f0c6b3cef5
Mikhail Gunin <gunka462@gmail.com>
2024-08-30 19:27:37 +0300

    Add `--default`

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 set -eo pipefail
 
+DEFAULT_TRAFFIC_DATA_DIR=/usr/local/etc/traffic-data
+
 trafficDataDir=$1
 
 if [ -z $trafficDataDir ]; then
     echo "Usage: $0 <traffic-data-dir>"
+    echo "Usage: $0 --default"
+    echo "(defaults to $DEFAULT_TRAFFIC_DATA_DIR)"
     exit 1
+fi
+
+if [ $trafficDataDir == '--default' ]; then
+    trafficDataDir=$DEFAULT_TRAFFIC_DATA_DIR
 fi
 
 sed "s|<traffic-data-dir>|$trafficDataDir|g" xray-stats.cron \
