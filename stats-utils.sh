e506ce0dcc5df7b153c1c73e973b03629eea9268
Mikhail Gunin <gunka462@gmail.com>
2024-11-26 08:52:03 +0300

    perf: remove subcommand usage

--- stats-utils.sh
+++ stats-utils.sh
@@ -1,16 +1,21 @@
 #!/bin/env bash
 
-get-traffic-data-directory() {
-    cat /usr/local/etc/xray-stats/directory
-}
+# shellcheck disable=SC2034 # Used in the sourcing scripts.
+TRAFFIC_DIR=$(</usr/local/etc/xray-stats/directory)
 
-sum() {
+sum-num-file() {
     local file=$1
-
-    if [ ! -f "$file" ]; then
+    if [[ ! -f "$file" ]]; then
         echo 0
         return
     fi
 
-    grep '^[0-9]\+$' "$file" | paste -sd+ - | bc
+    local total=0
+    while IFS= read -r line; do
+        if [[ "$line" =~ ^[0-9]+$ ]]; then
+            total=$((total + line))
+        fi
+    done < "$file"
+
+    echo "$total"
 }
