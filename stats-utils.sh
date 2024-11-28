44fe247f161d3c82fdded89ba80dc5d49f6379c5
Mikhail Gunin <gunka462@gmail.com>
2024-11-28 13:22:22 +0300

    perf: globbing with awk

--- stats-utils.sh
+++ stats-utils.sh
@@ -4,18 +4,6 @@
 TRAFFIC_DIR=$(</usr/local/etc/xray-stats/directory)
 
 sum-num-file() {
-    local file=$1
-    if [[ ! -f "$file" ]]; then
-        echo 0
-        return
-    fi
-
-    local total=0
-    while IFS= read -r line; do
-        if [[ "$line" =~ ^[0-9]+$ ]]; then
-            total=$((total + line))
-        fi
-    done < "$file"
-
-    echo "$total"
+    awk '/^[0-9]+$/ {sum += $1} END {print sum}' "$@" 2> /dev/null \
+        || echo 0
 }
